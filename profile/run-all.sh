eaf78ec15fbe2d184e95fbcc0627a9d21fadf47a
Joe Lin <hualee215@gmail.com>
2015-06-25 15:17:29 +0800

    can't run the app at linux, eg ubuntu
    update the shell for running at ubuntu. but i can't run it after update the shell.

--- profile/run-all.sh
+++ profile/run-all.sh
@@ -1,16 +1,18 @@
 
+basedir=$(pwd)
+
 cd ./database-h2-server
-nohup ./run.sh
-cd
+nohup ./run.sh &
+cd $basedir
 
 cd ./chat-central-server
 nohup ./run.sh
-cd
+cd $basedir
 
-cd ./chat-front-server\bin
+cd ./chat-front-server/bin
 nohup ./run.sh
-cd
+cd $basedir
 
-cd ./chat-node-server\bin
+cd ./chat-node-server/bin
 nohup ./run.sh
-cd
+cd $basedir
